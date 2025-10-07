f8316d931067b66e56ab35cd919179db66ee9187
grey-2078 <zharov.gd@gmail.com>
2025-10-07 11:48:37 +0300

    добавил докстринги

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -1,4 +1,8 @@
+"""
+код выводит заданный в переменную message текст через функцию text
+"""
 message = input()
 def text(message):
+    """возвращает записанный в функцию текст"""
     return message
 print(text(message))
