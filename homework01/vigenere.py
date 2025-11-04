dc51c8c2d0b7b4e218b43db6e79a739353836b36
grey-2078 <zharov.gd@gmail.com>
2025-11-04 10:33:59 +0300

    реализован шифр Виженера

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,3 +1,18 @@
+def generate_key(plaintext, keyword):
+    """
+    Generates a key for encryption.
+    >>> generate_key("ATTACKATDAWN", "LEMON")
+    'LEMONLEMONLE'
+    """
+    keyword = list(keyword)
+    if len(plaintext) == len(keyword):
+        return keyword
+    else:
+        for i in range(len(plaintext) - len(keyword)):
+            keyword.append(keyword[i % len(keyword)])
+    return "".join(keyword)
+
+
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
@@ -9,8 +24,19 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    keyword = generate_key(plaintext, keyword)
+    for i in range(len(plaintext)):
+        char = plaintext[i]
+        if char.isupper():
+            encrypted_char = chr((ord(char) + ord(keyword[i]) - 2 * ord('A')) % 26 + ord('A'))
+        elif char.islower():
+            encrypted_char = chr((ord(char) + ord(keyword[i]) - 2 * ord('a')) % 26 + ord('a'))
+        else:
+            encrypted_char = char
+        ciphertext += encrypted_char
+
     return ciphertext
+print(encrypt_vigenere("itmo", "itmo"))
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
@@ -24,5 +50,15 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    keyword = generate_key(ciphertext, keyword)
+    for i in range(len(ciphertext)):
+        char = ciphertext[i]
+        if char.isupper():
+            decrypted_char = chr((ord(char) - ord(keyword[i]) + 26) % 26 + ord('A'))
+        elif char.islower():
+            decrypted_char = chr((ord(char) - ord(keyword[i]) + 26) % 26 + ord('a'))
+        else:
+            decrypted_char = char
+        plaintext += decrypted_char
     return plaintext
+print(decrypt_vigenere("PYTHON", "ATTACKATDAWN"))
